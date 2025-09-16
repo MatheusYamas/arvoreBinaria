08cbab1e756809396e28201392dfef2cefee7542
Matheus Yamamoto Dias <matheusyamas@gmail.com>
2025-09-16 00:55:19 -0300

    Implementado inserir, remover menor, pre-ordem, por-ordem e in-ordem

--- src/arvoresbinaria/ArvoreBinaria.java
+++ src/arvoresbinaria/ArvoreBinaria.java
@@ -114,5 +114,6 @@ public class ArvoreBinaria {
         arvore.inOrdem();
         arvore.posOrdem();
         arvore.removerMenor();
+
     }
 }
--- src/arvoresbinaria/Node.java
+++ src/arvoresbinaria/Node.java
@@ -1,5 +1,4 @@
 package arvoresbinaria;
-
 public class Node {
     private Integer informacao;
     private Node esquerda;
